5cf3efb1705ff721119f5eb24e70fa71ea3a1c6d
nikish11111 <nikish11111@mail.ru>
2018-09-23 20:33:54 +0700

    add welcome message

--- src/main.c
+++ src/main.c
@@ -1,4 +1,15 @@
+#include <stdio.h>
+
+void welcome_message()
+{
+    printf("Hello. This is game \"Bulls and Cows\".\n");
+    printf("I conceived a 4-digit number.");
+    printf("Your task is to guess it by entering numbers.\n");
+    printf("If the number is the same, but it stands elsewhere - it's a cow,");
+    printf(" if it's in the same place - it's a bull.\n\n");
+}
+
 int main()
 {
-
+    welcome_message();
 }
